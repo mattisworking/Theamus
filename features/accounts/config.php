913c3c519ca3b623b69179c3900d7b735f81f81f
Eyrah Temet <matt.tempinski@gmail.com>
2014-08-06 14:37:29 -0500

    updated the config file to be able to login again

--- features/accounts/config.php
+++ features/accounts/config.php
@@ -1,35 +1,35 @@
 <?php
 
-// Define custom feature folders
-$feature['js']['folder']        = 'js';
-$feature['css']['folder']       = 'css';
-$feature['scripts']['folder']   = 'php';
-$feature['class']['folder']     = 'php';
+$Theamus->Call->set_feature_config(array(
+    // System Required
+    'theamus_version' => 1.0,
+    'permissions'     => array('database'),
 
-// Custom function file to load beforehand
-//$feature['functions']['file'] = 'php/functions.php';
-$feature['api']['class_file'] = array('php/accounts.class.php');
+    // Feature Required
+    'folder_name'     => 'accounts',
+    'feature_name'    => 'Theamus Accounts',
+    'feature_version' => 1.0,
+    'custom_folders'  => array(
+        'php'        => 'php',
+        'javascript' => 'js',
+        'css'        => 'css',
+        'class'      => 'php'
+    ),
 
-// Define feature information
-$feature['alias']       = 'accounts';
-$feature['name']        = 'Accounts';
-$feature['db_prefix']   = '';
-$feature['groups']      = array('everyone');
-$feature['version']     = '1.2';
-$feature['notes']       = array(
-    'Rewrote the entire feature from the ground up to work with the latest Theamus standards',
-    'Updated the feature styling to be more comfortable for users and to fit the new admin panel'
-);
+    // Feature Optional
+    'load_files' => array(
+        'api' => array('php/accounts.class.php')
+    ),
+    'release_notes' => array(
+        '1.1' => array('Update to work with the latest version of Theamus'),
+        '1.0' => array('Initial release.')
+    ),
 
-// Define the author information
-$feature['author']['name']      = 'Eyrah Temet';
-$feature['author']['alias']     = 'Eyraahh';
-$feature['author']['email']     = 'eyrah.temet@theamus.com';
-$feature['author']['company']   = 'Theamus';
-
-// Define configuration scripts
-/*
-$feature['install']['script']   = 'config/install.php';
-$feature['update']['script']    = 'config/update.php';
-$feature['remove']['script']    = 'config/remove.php';
- */
+    // Optional Author Information
+    'author' => array(
+        'name'    => array('Eyrah Temet'),
+        'alias'   => array('Eyraahh'),
+        'email'   => array('eyrah.temet@theamus.com'),
+        'company' => 'Theamus'
+    )
+));
