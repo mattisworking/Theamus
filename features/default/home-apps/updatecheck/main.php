24d304a79e33569e48dea34d11fc73e3f43e1398
Eyrah Temet <matt.tempinski@gmail.com>
2014-09-05 12:03:32 -0500

    fixes #84

--- features/default/home-apps/updatecheck/main.php
+++ features/default/home-apps/updatecheck/main.php
@@ -2,32 +2,20 @@
 
 $error = false; // Start out the day nicely.
 
-// Make the AJAX call to the update server
-$server = 'http://theamus.com/release-manager/update-info/';
-$info = $Theamus->API->api(array(
-    'type'  => 'get',
-    'url'   => $server,
-    'method'=> array('Releases', 'get_update_info'),
-    'key'   => 'dQPlembXjBfGvmCqH0Cot9uMeKAbRkTdr6ysWK1V50U='
-));
+// Get the update information from the server
+$json = file_get_contents("http://localhost/theamus/releases/api/update-info/");
+$info = json_decode($json, true);
 
 // Check the results
-if ($info['error']['status'] == 1) {
-    $error = 'There was an issue retrieving the data from the server';
+if ($info === null) {
+    echo 'There was an issue retrieving the data from the server';
 } else {
-    $update_data = $info['response']['data'];
-}
-
-if ($error != false) {
-    echo $error;
-} else {
-    if (is_array($update_data['old_versions'])) {
-        if (in_array($Theamus->settings['version'], $update_data['old_versions'])) {
-            notify('admin', 'success', 'There\'s an update available for your system!'.
-                ' - <a href="#" onclick="return admin_go(\'settings\', \'settings/settings/\');\">Update Now!</a>');
+    if (is_array($info['old_versions'])) {
+        if (in_array($Theamus->settings['version'], $info['old_versions'])) {
+            $Theamus->notify("success", "There's an update available! - <a href='#' id='updatecheck_update-link'>Update Now!</a>");
         ?>
-            <div style='border: 1px solid #EEE; margin: 10px 0; padding: 5px 5px 10px; overflow-y: auto; max-height: 150px;'>
-                <?=$update_data['notes']?>
+            <div style='border: 1px solid #EEE; margin: 10px 0; padding: 5px 5px 10px; overflow-y: auto; max-height: 250px;'>
+                <?php echo $Theamus->Parsedown->text($info['notes']); ?>
             </div>
         <?php
         } else {
@@ -37,3 +25,9 @@ if ($error != false) {
         echo 'There was an issue gathering update data.';
     }
 }
+
+?>
+
+<script>
+    admin_window_run_on_load("updatecheck_main");
+</script>
